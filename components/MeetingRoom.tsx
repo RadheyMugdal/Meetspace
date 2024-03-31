d44951bcdcda3b591b70c3d6e5377c77cc3db46b
Radhey Mugdal <mugdalradhey@gmail.com>
2024-03-31 16:53:42 +0530

    recordings,persional room,previous done

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -10,7 +10,7 @@ import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu"
 
 import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from "@stream-io/video-react-sdk"
 import { LayoutList, Users } from "lucide-react"
-import { useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { useState } from "react"
 import EndCallButton from "./EndCallButton"
 import Loader from "./Loader"
@@ -21,7 +21,7 @@ const MeetingRoom = () => {
   const isPersonalRoom=!!searchParams.get('persional')
   const {useCallCallingState}=useCallStateHooks()
   const callingState=useCallCallingState()
-  
+  const router=useRouter()
   const [showParticipants,setShowParticipants]=useState(false)
   const CallLayout=()=>{
     switch (layout) {
@@ -44,7 +44,7 @@ const MeetingRoom = () => {
     <section className=" relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className=" relative flex size-full items-center justify-center">
         <div className=" flex size-full max-w-[1000px] items-center">
-        <CallLayout/>
+        <CallLayout  />
         </div>
         <div className={cn(" h-[calc(100vh-86px)] hidden ml-2",{'show-block':showParticipants})}>
           <CallParticipantsList onClose={()=>setShowParticipants(false)} />
@@ -52,7 +52,7 @@ const MeetingRoom = () => {
         </div>
       </div>
       <div className=" fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls  />
+        <CallControls onLeave={()=> router.push('/')}  />
         <DropdownMenu>
           <div className=" flex items-center" >
 
