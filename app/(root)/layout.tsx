20e8a0842948942389bb231307accca287496010
Radhey Mugdal <mugdalradhey@gmail.com>
2024-03-31 12:14:43 +0530

    Home page design and schedule and instany  meeting work done

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+
+import React from "react";
+export const metadata: Metadata = {
+  title: "Meetspace",
+  description: "Video Calling App",
+
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+const RootLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+      {children}
+      </StreamVideoProvider>
+
+    </main>
+  )
+}
+
+export default RootLayout
---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import 'react-datepicker/dist/react-datepicker.css';
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Meetspace",
+  description: "Video Calling App",
+
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider appearance={{
+        layout:{
+          logoImageUrl:'/icons/yoom-logo.svg',
+          socialButtonsVariant:"iconButton"
+        },
+        variables:{
+          colorText:'#fff',
+          colorPrimary:"#0E78F9",
+          colorBackground:"#1c1f2e",
+          colorInputBackground:"#253a41",
+          colorInputText:"#fff"
+
+        }
+      }}>
+
+      <body className={`${inter.className} bg-dark-2 text-white`}>{children}
+      <Toaster />
+      </body>
+      </ClerkProvider>
+    </html>
+  );
+}
