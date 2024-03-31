ee7a13a248d78fa5ec7d517055a7e74a502c7e93
Radhey Mugdal <mugdalradhey@gmail.com>
2024-03-31 18:28:13 +0530

    Navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className=" flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10" >
       <Link href="/" className=" flex items-center gap-1" >
-        <Image src="/icons/logo.svg" width={32} height={32} alt="Yoom Logo" className=" max-sm:size-10" />
+        <Image src="/icons/logo.svg" width={32} height={32} alt="meetspace Logo" className=" max-sm:size-10" />
         <p className=" text-[26px] font-extrabold text-white max-sm:hidden">Meetspace</p>
       </Link>
       <div className=" flex-between gap-5">
